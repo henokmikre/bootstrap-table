bac566c374c58708a3f98e0abaa6f8f774d4fd93
Henok Mikre <henok@blencorp.com>
2017-08-31 13:13:33 -0400

    Reverted c88835421775200737219c920c485915e50a9672.
    We need the tabindex in order to sort with keyboard.
    This was removed in
    https://github.com/wenzhixin/bootstrap-table/issues/2719.

--- dist/bootstrap-table.js
+++ dist/bootstrap-table.js
@@ -811,6 +811,7 @@
                     sprintf(' rowspan="%s"', column.rowspan),
                     sprintf(' colspan="%s"', column.colspan),
                     sprintf(' data-field="%s"', column.field),
+                    "tabindex='0'",
                     '>');
 
                 html.push(sprintf('<div class="th-inner %s">', that.options.sortable && column.sortable ?
